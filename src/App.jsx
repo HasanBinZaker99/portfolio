747873b66902ff1c2c1f8fb03fab7c5269144da8
Mohammed Hasan Bin Zaker <57794851+HasanBinZaker99@users.noreply.github.com>
2024-12-18 16:48:20 -0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
-import React from "react";
 
+
+import styles from "./App.module.css";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
-
-
-// import styles from "./App.module.css";
-// import { Hero } from "./components/Hero/Hero";
-// import { Navbar } from "./components/Navbar/Navbar";
-// import { About } from "./components/About/About";
-// import { Experience } from "./components/Experience/Experience";
-// import { Projects } from "./components/Projects/Projects";
-// import { Contact } from "./components/Contact/Contact";
-// function App() {
-//   return (
-//     <div className={styles.App}>
-//       <Navbar />
-//       <Hero />
-//       <About />
-//       <Experience />
-//       <Projects />
-//       <Contact />
-//     </div>
-//   );
-// }
-
-// export default App;
