529f1030801d2d60f37c2a12c3927070d95eba00
Md Hasan Zaker <mdhasanzaker@mac.home>
2024-12-18 14:12:21 -0330

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h4 className={styles.title}>
+          Assalamu alaykum (peace , mercy and blessing of Allah be upon you){" "}
+        </h4>
+        <p className={styles.description}>
+          I'm a full-stack developer with 5 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:mdhbz99c@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
