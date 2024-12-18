0f3144ecc1d87e76b5d18aa8801333ea4382a0a5
Mohammed Hasan Bin Zaker <57794851+HasanBinZaker99@users.noreply.github.com>
2024-12-18 15:44:30 -0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
+  base: "/portfolio/", // <-- Replace with your GitHub repository name
 });
