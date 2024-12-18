5494223f96f99fe64ac2ca99a63478fd18827471
Mohammed Hasan Bin Zaker <57794851+HasanBinZaker99@users.noreply.github.com>
2024-12-18 15:56:45 -0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import "@fontsource/outfit";
-import "@fontsource/roboto";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import "@fontsource/outfit";
+// import "@fontsource/roboto";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
